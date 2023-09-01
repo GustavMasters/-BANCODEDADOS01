d34bd32ee51251790191bc80e3b9dbbaca94353e
GustavMasters <143405160+GustavMasters@users.noreply.github.com>
2023-09-01 20:28:44 -0300

    Aula 06

--- pessoa.py
+++ pessoa.py
@@ -1,40 +1,37 @@
-import psycopg2 as db
+import psycopg2 as bd
 import csv
 
-
-# criar classe de configuração e método de inicialização
+# Criar classe de configuração e método de inicialização
 class Config:
     def __init__(self):
         self.config = {
             "postgres": {
                 "user": "postgres",
-                "password": "123",
+                "password": "121316",
                 "host": "127.0.0.1",
                 "port": "5432",
                 "database": "pydb"
             }
         }
 
-
-# Criar uma classe conexão para receber um herança de Config
-# ** desempacotar
+# Criar uma classe conexão para receber uma herança de Config
 class Connection(Config):
     def __init__(self):
         # Chama o construtor da classe base usando super()
         super().__init__()
         try:
             # Cria a conexão e o cursor
-            self.conn = db.connect(**self.config["postgres"])
+            self.conn = bd.connect(**self.config["postgres"])
             self.cur = self.conn.cursor()
         except Exception as e:
             print("Erro na conexão", e)
             exit(1)
 
-    # métodos de retorno a própria classe
+    # Métodos de retorno à própria classe
     def __enter__(self):
         return self
 
-    # toda vez que sair da classe deverá sair da conexão
+    # Toda vez que sair da classe, deverá sair da conexão    
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.commit()
         self.connection.close()
@@ -51,20 +48,19 @@ class Connection(Config):
     def commit(self):
         self.connection.commit()
 
-    # fetchall retornará em determinados registros que será passado
+    # Fetchall retornará em determinados registros que será passado 
     # instrução SQL
     def fetchall(self):
         return self.cursor.fetchall()
 
-    # Método receberá a instrução SQL passará parâmetros ou tuplas vazias
+    # Método receberá a instrução SQL e passará parâmetros ou tuplas vazias
     def execute(self, sql, params=None):
         self.cursor.execute(sql, params or ())
 
-    # Cria uma instrução por meio de uma consulta(query)
+    # Cria uma instrução por meio de uma consulta (query)
     def query(self, sql, params=None):
         self.cursor.execute(sql, params or ())
         return self.fetchall()
-
 
 # Criar a Classe Pessoa
 class Pessoa(Connection):
@@ -72,40 +68,42 @@ class Pessoa(Connection):
         # Chama o construtor da classe base usando super()
         super().__init__()
 
-    # métodos de manipulação de registros
+    # Métodos de manipulação de registros
     def insert(self, *args):
         try:
             sql = "INSERT INTO pessoa (nome) VALUES (%s)"
             # Usa o método execute da classe base
             self.execute(sql, args)
             self.commit()
+            print("Registro Inserido")
         except Exception as e:
             print("Erro ao inserir", e)
 
-    # método inserir_csv
+    # Método inserir_csv
     def insert_csv(self, filename):
         try:
             data = csv.DictReader(open(filename, encoding="utf-8"))
             for row in data:
                 self.insert(row["nome"])
-                print("Registro Inserido")
+            print("Registros Inseridos")
         except Exception as e:
             print("Erro ao inserir csv", e)
 
-    # método delete
+    # Método delete
     def delete(self, id):
         try:
             sql_s = f"SELECT * FROM pessoa WHERE id = {id}"
             if not self.query(sql_s):
-                return "Registro não encontrado para deletar"
+                print("Registro não encontrado para deletar")
+                return
             sql_d = f"DELETE FROM pessoa WHERE id = {id}"
             self.execute(sql_d)
             self.commit()
-            return "Registro deletado"
+            print("Registro deletado")
         except Exception as e:
             print("Erro ao deletar", e)
 
-    # método update
+    # Método update
     def update(self, id, *args):
         try:
             sql = f"UPDATE pessoa SET nome = %s WHERE id = {id}"
@@ -115,16 +113,10 @@ class Pessoa(Connection):
         except Exception as e:
             print("Erro ao atualizar", e)
 
-
-# Testar Instâncias
 if __name__ == "__main__":
     pessoa = Pessoa()
-    pessoa.insert("João")
+    # Exemplo de uso:
+    pessoa.insert("angela")
     pessoa.insert_csv("data.csv")
-    # pessoa.delete(10)
-    # pessoa.update(6, "Roberto")
-    # pessoa.insert("Luis")
-    # pessoa.insert_csv("data.csv")
-    # pessoa.delete(10)
-    # pessoa.update(6, "Roberto")
-
+    pessoa.delete(4)
+    pessoa.update(1, "Maria Silva")
